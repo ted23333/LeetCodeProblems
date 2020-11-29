72ea40b5525f37c2a761a954781d938f6fb0d6e7
ted23333 <dongxuanhe@qq.com>
2020-11-29 09:03:23 +0800

    # 1129update

---
+++ LeetCodeProblems/src/Leetcode55/Solution.java
@@ -0,0 +1,23 @@
+package Leetcode55;
+// 跳跃游戏
+  public class Solution {
+        public boolean canJump(int[] nums) {
+            int n = nums.length;
+            int rightmost = 0;
+            for (int i = 0; i < n; ++i) {
+                if (i <= rightmost) {
+                    // rightmost是能够到达的最大位置
+                    rightmost = Math.max(rightmost, i + nums[i]);
+                    // 如果遍历到的位置能够到达的最大位置大于当前的最大位置，则更新最大的位置
+                    if (rightmost >= n - 1) {
+                        return true;
+                        // 如果能够遍历数组的最后一个位置，那么就直接返回true
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+
+
