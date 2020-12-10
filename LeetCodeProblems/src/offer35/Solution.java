711e1309553417aef1383493a5d5b8e7d7c543c4
ted23333 <dongxuanhe@qq.com>
2020-12-10 20:04:00 +0800

    #1210update

---
+++ LeetCodeProblems/src/offer35/Solution.java
@@ -0,0 +1,41 @@
+package offer35;
+// 深拷贝结点
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        Node cur = head;
+        Map<Node, Node> map = new HashMap<>();
+        // 3. 复制各节点，并建立 “原节点 -> 新节点” 的 Map 映射
+        while(cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        cur = head;
+        // 4. 构建新链表的 next 和 random 指向
+        while(cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        //这里使用哈希表的原因是因为多了一个random结点
+        }
+
+        // 5. 返回新链表的头节点
+        return map.get(head);
+    }
+
+
+}
