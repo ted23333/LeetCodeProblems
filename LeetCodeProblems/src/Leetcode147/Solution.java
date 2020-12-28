ba906c1cbe31da2b7ba0b3d5d3fefff9744dfb0a
ted23333 <dongxuanhe@qq.com>
2020-12-28 10:53:46 +0800

    #1228update

---
+++ LeetCodeProblems/src/Leetcode147/Solution.java
@@ -0,0 +1,43 @@
+package Leetcode147;
+// 排序列表
+class Solution {
+      public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tmp = slow.next;
+        slow.next = null;
+        // 将链表断开
+        ListNode left = sortList(head);
+        ListNode right = sortList(tmp);
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        // 建立新的链表的头结点
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        return res.next;
+        //交替添加元素
+    }
+}
+
+
