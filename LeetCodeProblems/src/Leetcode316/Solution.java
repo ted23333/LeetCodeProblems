f7f502402726f16e61dc814fe176b49ceb2b082c
ted23333 <dongxuanhe@qq.com>
2021-01-19 10:57:21 +0800

    #0119update

---
+++ LeetCodeProblems/src/Leetcode316/Solution.java
@@ -0,0 +1,34 @@
+package Leetcode316;
+// 去掉重复字母
+// 给定一个字符串s，去除字符串中重复的字母使得每个字母只出现一次，需要保证返回结果的字典序最小
+// 没看懂这个题
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        boolean[] vis = new boolean[26];
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+        // 这里先得到每一个字母的个数
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!vis[ch - 'a']) {
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        vis[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+                vis[ch - 'a'] = true;
+                sb.append(ch);
+            }
+            num[ch - 'a'] -= 1;
+        }
+        return sb.toString();
+    }
+}
+
+
