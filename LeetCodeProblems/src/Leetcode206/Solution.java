902a7764a72ad74a8a704b14a579a1818234994c
ted23333 <dongxuanhe@qq.com>
2021-02-08 17:16:03 +0800

    #0208update

--- LeetCodeProblems/src/Leetcode206/Solution.java
+++ LeetCodeProblems/src/Leetcode206/Solution.java
@@ -17,26 +17,16 @@ class Solution {
         }
     }
     public ListNode reverseList(ListNode head) {
-        if(head==null) return null;
-        if(head.next==null) return head;
-        ListNode dummyHead = new ListNode(0);
-        dummyHead.next=head;
-        ListNode end = head;
-        ListNode temp;
-        while(end.next!=null)
-        {
-            end = end.next;
-            //让end移动到最后一个节点
-        }
-        while(dummyHead.next!=end)
-        {
-            temp = dummyHead.next;
-            dummyHead.next=temp.next;
-           temp.next = end.next;
-           end.next = temp;
-            //最后一个结点的下一个结点是虚拟头结点的下一个结点
-
-        }
-        return dummyHead.next;
-    }
+      ListNode prev = null;
+      ListNode curr = head;
+      while(curr!=null){
+          ListNode next = curr.next;
+          //提前存储当前结点的下一个结点
+          curr.next = prev;
+          // 把指针反转
+          curr= next;
+          // 推进指针到下一个
+      }
+      return prev;
+}
 }
