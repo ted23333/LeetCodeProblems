b5ea5a9c848923600d3e3ded8f29d29334255a66
ted23333 <dongxuanhe@qq.com>
2021-02-09 17:43:10 +0800

    #0209update

---
+++ LeetCodeProblems/src/Leetcode129/Solution.java
@@ -0,0 +1,35 @@
+package Leetcode129;
+
+
+
+public class Solution {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }}
+      //深度优先遍历
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode root, int prevSum) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = prevSum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        } else {
+            return dfs(root.left, sum) + dfs(root.right, sum);
+        }
+    }
+
+
+}
