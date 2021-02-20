ad8ad97fd23ca031d9ee6845f3ba8568f4830645
ted23333 <dongxuanhe@qq.com>
2021-02-20 10:52:04 +0800

    # 0220 update

---
+++ LeetCodeProblems/src/Leetcode450/Solution.java
@@ -0,0 +1,18 @@
+package Leetcode450;
+// 删除二叉搜索树中的节点
+public class Solution {
+    class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }}
+    public TreeNode deleteNode(TreeNode root, int key) {
+
+    }
+}
---
+++ LeetCodeProblems/src/Leetcode572/Solution.java
@@ -0,0 +1,40 @@
+package Leetcode572;
+// 另一个树的子树
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+      class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }}
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (t == null) return true;   // t 为 null 一定都是 true
+        if (s == null) return false;  // 这里 t 一定不为 null, 只要 s 为 null，肯定是 false
+        return isSubtree(s.left, t) || isSubtree(s.right, t) || isSameTree(s,t);
+    }
+
+    /**
+     * 判断两棵树是否相同
+     */
+    public boolean isSameTree(TreeNode s, TreeNode t){
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        if (s.val != t.val) return false;
+        return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+    }
+}
+
