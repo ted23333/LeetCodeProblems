7d02886e7d718de1b38d048a977fe37081a1d18a
ted23333 <dongxuanhe@qq.com>
2021-02-25 16:39:08 +0800

    # 0225 update

--- LeetCodeProblems/src/offer62/Solution.java
+++ LeetCodeProblems/src/offer62/Solution.java
@@ -1,5 +1,23 @@
 package offer62;
-// 约瑟夫环问题
-public class Solution {
 
+import java.util.ArrayList;
+
+// 约瑟夫环问题
+class Solution {
+    public int lastRemaining(int n, int m) {
+        ArrayList<Integer> list = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        // n个数字
+        int idx = 0;
+        while (n > 1) {
+            idx = (idx + m - 1) % n;
+            list.remove(idx);
+            n--;
+        }
+        return list.get(0);
+    }
 }
+
+
---
+++ LeetCodeProblems/src/offer62/Solution2.java
@@ -0,0 +1,14 @@
+package offer62;
+// 约瑟夫环的数学解法
+class Solution2 {
+    public int lastRemaining(int n, int m) {
+        int ans = 0;
+        // 最后一轮剩下2个人，所以从2开始反推
+        for (int i = 2; i <= n; i++) {
+            ans = (ans + m) % i;
+        }
+        return ans;
+    }
+}
+
+
