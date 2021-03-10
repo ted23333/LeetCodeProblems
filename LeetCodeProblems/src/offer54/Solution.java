94b228c58e36261af4b1457e855c5fc67b51a925
ted23333 <dongxuanhe@qq.com>
2021-03-10 09:00:50 +0800

    # 0310 update

---
+++ LeetCodeProblems/src/offer54/Solution.java
@@ -0,0 +1,32 @@
+package offer54;
+// 二叉搜索树的第k大的节点
+public class Solution {
+            public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    int res, k;
+    public int kthLargest(TreeNode root, int k) {
+        this.k = k;
+        dfs(root);
+        return res;
+    }
+    // 二叉搜索树的中序倒序遍历是一个递减序列
+    void dfs(TreeNode root) {
+        if(root == null) return;
+        dfs(root.right);
+        if(k == 0) return;
+        if(--k == 0) res = root.val;
+        dfs(root.left);
+    }
+
+
+}
