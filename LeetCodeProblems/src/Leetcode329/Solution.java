dac7469accefedbfc58aa3cafcfc29b9b0442c51
ted23333 <dongxuanhe@qq.com>
2021-06-21 16:15:05 +0800

    # 0621update

---
+++ LeetCodeProblems/src/Leetcode329/Solution.java
@@ -0,0 +1,40 @@
+package Leetcode329;
+// 矩阵中的最长递增路径
+class Solution {
+    public int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    public int rows, columns;
+
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        // 检查异常条件
+        rows = matrix.length;// 行数
+        columns = matrix[0].length; // 列数
+        int[][] memo = new int[rows][columns]; // 记忆化搜索
+        int ans = 0;
+        // 记录当前的最长递增路径
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
+                ans = Math.max(ans, dfs(matrix, i, j, memo));
+            }
+        }
+        return ans;
+    }
+
+    public int dfs(int[][] matrix, int row, int column, int[][] memo) {
+        if (memo[row][column] != 0) {
+            return memo[row][column];
+        }
+        ++memo[row][column];
+        for (int[] dir : dirs) {
+            int newRow = row + dir[0], newColumn = column + dir[1];
+            if (newRow >= 0 && newRow < rows && newColumn >= 0 && newColumn < columns && matrix[newRow][newColumn] > matrix[row][column]) {
+                memo[row][column] = Math.max(memo[row][column], dfs(matrix, newRow, newColumn, memo) + 1);
+            }
+        }
+        return memo[row][column];
+    }
+}
+
+
