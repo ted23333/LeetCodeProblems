c9543a4252b188397b89d2611092f7550e2f31bc
hsuan-he <tedong1998@foxmail.com>
2021-07-25 21:16:45 +0800

    # 0725update

--- LeetCodeProblems/src/Leetcode134/Solution.java
+++ LeetCodeProblems/src/Leetcode134/Solution.java
@@ -7,11 +7,13 @@ class Solution {
 
         // 从头到尾遍历每个加油站，并且检查以该加油站为起点，能否行驶一周
         while(i < n){
+            // 从第一个开始计算
             int sumOfGas  = 0; // 总共加的油
             int SumOfCost = 0; // 总共消费的油
             int count = 0;     // 记录能走过几个站点
             while(count < n){  // 退出循环的条件是走过所有的站点
                 int j = (i + count) % n; // 加油站是环形的
+                // j代表当前所在的站点
                 sumOfGas += gas[j];
                 SumOfCost += cost[j];
                 if(SumOfCost > sumOfGas){ // 如果这个站点发现油不够了
@@ -25,6 +27,7 @@ class Solution {
             }else{ // 不行的话 从下一个站点开始 检查
                 i = i + count + 1;
             }
+            // 肯定中间的节点都不用看了，因为从头开始到不了的话，从后面更是到不了
         }
         // 所有加油站作为起点都不满足
         return -1;
