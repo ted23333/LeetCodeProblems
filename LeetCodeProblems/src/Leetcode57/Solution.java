20e1fd2913cd440ebc55b9a8e5da3eff9b5c7650
hsuan-he <tedong1998@foxmail.com>
2021-07-29 13:55:35 +0800

    # 0729update

--- LeetCodeProblems/src/Leetcode57/Solution.java
+++ LeetCodeProblems/src/Leetcode57/Solution.java
@@ -8,13 +8,16 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int left = newInterval[0];
         int right = newInterval[1];
+        // 新的区间左边界和右边界
         boolean placed = false;
         List<int[]> ansList = new ArrayList<int[]>();
+        // 新的arrayList
         for (int[] interval : intervals) {
             if (interval[0] > right) {
                 // 在插入区间的右侧且无交集
                 if (!placed) {
                     ansList.add(new int[]{left, right});
+                    // 如果待插入区间在某个区间的右边，先插入区间，然后把已知的区间插入
                     placed = true;
                 }
                 ansList.add(interval);
@@ -26,13 +29,17 @@ class Solution {
                 left = Math.min(left, interval[0]);
                 right = Math.max(right, interval[1]);
             }
+            // 相当于简化成三种情况，两种是没有交集的，一种是有交集的
         }
         if (!placed) {
             ansList.add(new int[]{left, right});
-        }
+            // 如果是有交集的话就要看是不是和下一个区间也有关联
+        }// 使用placed记录状态
         int[][] ans = new int[ansList.size()][2];
+       //  返回一个二维数组
         for (int i = 0; i < ansList.size(); ++i) {
             ans[i] = ansList.get(i);
+            // 可以按照维度赋值
         }
         return ans;
     }
---
+++ LeetCodeProblems/src/leetcode60/Solution.java
@@ -0,0 +1,83 @@
+package leetcode60;
+// 排列序列
+import java.util.Arrays;
+
+public class Solution {
+
+    /**
+     * 记录数字是否使用过
+     */
+    private boolean[] used;
+
+    /**
+     * 阶乘数组
+     */
+    private int[] factorial;
+
+    private int n;
+    private int k;
+
+    public static void main(String[] args) {
+        String so = new Solution().getPermutation(4,9);
+    }
+
+    public String getPermutation(int n, int k) {
+        this.n = n;
+        this.k = k;
+        calculateFactorial(n);
+
+        // 查找全排列需要的布尔数组
+        used = new boolean[n + 1];
+        Arrays.fill(used, false);
+
+        StringBuilder path = new StringBuilder();
+        dfs(0, path);
+        // 开始深度优先遍历
+        return path.toString();
+    }
+
+
+    /**
+     * @param index 在这一步之前已经选择了几个数字，其值恰好等于这一步需要确定的下标位置
+     * @param path
+     */
+    private void dfs(int index, StringBuilder path) {
+        if (index == n) {
+            return;
+        }
+        // Index范围是0到n-1
+
+        // 计算还未确定的数字的全排列的个数，第 1 次进入的时候是 n - 1
+        int cnt = factorial[n - 1 - index];
+        for (int i = 1; i <= n; i++) {
+            if (used[i]) {
+                continue;
+            }
+            if (cnt < k) {
+                k -= cnt;
+                continue;
+                // 如果k大于cnt的数字，说明当前数字开头的不用放进去了
+            }
+            path.append(i);
+            used[i] = true;
+            dfs(index + 1, path);
+            // 注意 1：不可以回溯（重置变量），算法设计是「一下子来到叶子结点」，没有回头的过程
+            // 注意 2：这里要加 return，后面的数没有必要遍历去尝试了
+            return;
+        }
+    }
+
+    /**
+     * 计算阶乘数组
+     *
+     * @param n
+     */
+    private void calculateFactorial(int n) {
+        factorial = new int[n + 1];
+        factorial[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+    }
+}
+
