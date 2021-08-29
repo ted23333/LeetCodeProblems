1a5a34d3fbbcd87935a28dddf9aa139fa6fc3524
hsuan-he <tedong1998@foxmail.com>
2021-08-29 10:31:03 +0800

    # 0829update

--- LeetCodeProblems/src/offer19/Solution.java
+++ LeetCodeProblems/src/offer19/Solution.java
@@ -1,4 +1,46 @@
 package offer19;
+// 使用f[i][j]表示s的前i个字符和p中的前j个字符是否匹配
+/*
+        如果匹配的是小写字母，就应该满足f[i][j]=f[i-1][j-1]
+        如果匹配的是* 可以对p的第j-1个字符匹配任意自然数次数
 
-public class Solution {
+*/
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] f = new boolean[m + 1][n + 1];
+        // 定义dp矩阵
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    //如果是×号，看匹配情况，如果等于的话
+                    // 要么舍弃当前的字符匹配前一个，相当于×号无用 f i j-2
+                    // 要么向前匹配f i-1 j 类似于匹配多个
+                    f[i][j] = f[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        f[i][j] = f[i][j] || f[i - 1][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        f[i][j] = f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
+    }
+
+    public boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
+    }
 }
+
---
+++ LeetCodeProblems/src/offer20/Solution.java
@@ -0,0 +1,44 @@
+package offer20;
+
+import java.util.HashMap;
+import java.util.Map;
+//状态自动机
+class Solution {
+    public boolean isNumber(String s) {
+        Map[] states = {
+                new HashMap<>() {{ put(' ', 0); put('s', 1); put('d', 2); put('.', 4); }}, // 0.
+                new HashMap<>() {{ put('d', 2); put('.', 4); }},                           // 1.
+                new HashMap<>() {{ put('d', 2); put('.', 3); put('e', 5); put(' ', 8); }}, // 2.
+                new HashMap<>() {{ put('d', 3); put('e', 5); put(' ', 8); }},              // 3.
+                new HashMap<>() {{ put('d', 3); }},                                        // 4.
+                new HashMap<>() {{ put('s', 6); put('d', 7); }},                           // 5.
+                new HashMap<>() {{ put('d', 7); }},                                        // 6.
+                new HashMap<>() {{ put('d', 7); put(' ', 8); }},                           // 7.
+                new HashMap<>() {{ put(' ', 8); }}                                         // 8.
+        };
+        int p = 0;
+        char t;
+        for(char c : s.toCharArray()) {
+            if(c >= '0' && c <= '9') t = 'd';
+            else if(c == '+' || c == '-') t = 's';
+            else if(c == 'e' || c == 'E') t = 'e';
+            else if(c == '.' || c == ' ') t = c;
+            else t = '?';
+            if(!states[p].containsKey(t)) return false;
+            p = (int)states[p].get(t);
+        }
+        return p == 2 || p == 3 || p == 7 || p == 8;
+    }
+}
+/*
+0 起始的空格
+1 e之前的sign
+2 dot之前的digit
+3 dot之后的digit
+4 当dot前为空时，dot后的digit
+5 e
+6 e之后的sign
+7 e之后的digit
+8 尾部的blank
+ */
+
