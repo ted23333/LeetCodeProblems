edaadad94be107c52f1d202af323e644738eadbd
hsuan-he <tedong1998@foxmail.com>
2021-11-07 10:42:06 +0800

    # 1107update

--- LeetCodeProblems/src/Leetcode38/Solution.java
+++ LeetCodeProblems/src/Leetcode38/Solution.java
@@ -1,34 +1,26 @@
 package Leetcode38;
 // 外观数组
 // 这道题的的java解法可能有点问题
-public class Solution {
-
-
-        public String countAndSay(int n) {
-            // 递归终止条件
-            if (n == 1) {
-                return "1";
-            }
-            StringBuffer res = new StringBuffer();
-            // 拿到上一层的字符串
-            String str = countAndSay(n - 1);
-            int length = str.length();
-            // 开始指针为0
+class Solution {
+    public String countAndSay(int n) {
+        String str = "1";
+        for (int i = 2; i <= n; ++i) {
+            StringBuilder sb = new StringBuilder();
             int start = 0;
-            // 注意这从起始条件要和下面长度统一
-            for (int i = 1; i < length + 1; i++) {
-                // 字符串最后一位直接拼接
-                if (i == length) {
-                    res.append(i - start).append(str.charAt(start));
-                    // 直到start位的字符串和i位的字符串不同，拼接并更新start位
-                } else if (str.charAt(i) != str.charAt(start) ) {
-                    res.append(i - start).append(str.charAt(start));
-                    start = i;
+            int pos = 0;
+
+            while (pos < str.length()) {
+                while (pos < str.length() && str.charAt(pos) == str.charAt(start)) {
+                    pos++;
                 }
+                sb.append(Integer.toString(pos - start)).append(str.charAt(start));
+                start = pos;
+                // pos向前推进位置 start是前一个字符的位置
             }
-            return res.toString();
+            str = sb.toString();
+            /*每次更新str*/
         }
 
-
-
+        return str;
+    }
 }
