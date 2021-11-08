83987572a5f66ae6803167a4ea30d3f6529bb8e8
hsuan-he <tedong1998@foxmail.com>
2021-11-08 11:09:51 +0800

    # 1108update

---
+++ LeetCodeProblems/src/Leetcode41/Solution2.java
@@ -0,0 +1,25 @@
+package Leetcode41;
+// 将所有小于等于0的数字修改为n+1 避免产生干扰
+class Solution2 {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+}
+
