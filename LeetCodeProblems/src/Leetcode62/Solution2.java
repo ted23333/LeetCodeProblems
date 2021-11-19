f14d3345e24c08d858c0df762bd3fad1365c840f
ted23333 <dongxuanhe@qq.com>
2021-11-19 18:42:43 +0800

    # 1119update

---
+++ LeetCodeProblems/src/Leetcode62/Solution2.java
@@ -0,0 +1,18 @@
+package Leetcode62;
+
+import java.util.Arrays;
+
+public class Solution2 {
+    public int uniquePaths(int m, int n) {
+        int []dp= new int[n];
+        Arrays.fill(dp,1);
+        for(int i = 1;i<m;i++)
+        {
+            for(int j = 1;j<n;j++)
+            {
+                dp[j] = dp[j]+dp[j-1];
+            }
+        }
+        return dp[n-1];
+    }
+}
